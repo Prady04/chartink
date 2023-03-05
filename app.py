2c37fd4091fdfc5030d2010fd0dba6775722d101
prady04 <pradyhangsout@gmail.com>
2023-03-05 19:54:42 +0530

    UI

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
     
 @app.route('/')
 def index():
-    return render_template('main.html')
+    return render_template('base.html')
 
 @app.route("/eod", methods=["GET"])
 def scrape(): 
