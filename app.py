56b5eb9df8951e5a0ae7d01a1f26cc071810c156
prady04 <pradyhangsout@gmail.com>
2023-03-14 19:01:37 +0530

    update

--- app.py
+++ app.py
@@ -149,8 +149,8 @@ def investment():
 
 @app.route('/swing')
 def swing(): 
-    stocks =['ZYDUSLIFE','GENCON','DATAPATTNS','UTIAMC','BRITTANIA','JSL']
-    dat = "14-MAR-2023"  
+    stocks =['VLSFINANCE','CAMPUS']
+    dat = "15-MAR-2023"  
     message = "*** market breadth - extreme bearish => prefer reversal trades *****"   
     return(render_template('swing.html', stocks = stocks, dat=dat, message = message))
 ''' 
